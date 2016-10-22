6a96d2c0c94bf470e5bb5d923cdf4c73f3656668
Andreas Lorensen <andlo@outlook.dk>
2016-10-22 21:56:20 +0200

    Just drafting ad testing
    ...

---
+++ Draft1.ps1
@@ -0,0 +1,19 @@
+﻿#requires -Version 3.0 -Modules ActiveDirectory
+$username = 'alo@capto.dk' 
+$token = 'OAEkQmDl9UkSisxBHmwMGZWDQw9iKjkR91SIOXSC'
+$params = @{
+  Uri     = ' https://captosupport.zendesk.com/api/v2/tickets.json'
+  Method  = 'Get'
+  Headers = @{
+    Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($username)/token:$($token)"))
+  }
+} 
+$result = Invoke-RestMethod -Uri $params.Uri -Method $params.Method -Headers $params.Headers 
+$result.tickets |
+Where-Object -FilterScript {
+  $_.Status -ne 'normal'
+} |
+Select-Object -Property id , subject , Description , Priority , status |
+Sort-Object -Property priority |
+Out-host
+
