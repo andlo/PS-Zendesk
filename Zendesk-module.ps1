87ea81fabed847836979e5f1a5d653843e32008d
Andreas Lorensen <andlo@outlook.dk>
2016-10-23 20:48:51 +0200

    Added Search-Zendesk
    ...

--- Zendesk-module.ps1
+++ Zendesk-module.ps1
@@ -140,3 +140,93 @@ function Get-ZendeskTicket
   {
   }
 }
+
+<#
+    .Synopsis
+    Searching Zendesk
+    
+    .DESCRIPTION
+    Nothing yet...
+
+    .EXAMPLE
+    Search-Zendesk -ZendeskConection <Connection> -query 3245227 
+
+    The ticket with the id 3245227
+    .EXAMPLE 
+    Search-Zendesk -ZendeskConection <Connection> -query Greenbriar
+
+    Any record with the word Greenbriar
+
+    .EXAMPLE 
+    Search-Zendesk -ZendeskConection <Connection> -query type:user "Jane Doe"
+
+    User records with the exact string "Jane Doe"
+
+    .EXAMPLE
+    Search-Zendesk -ZendeskConection <Connection> -query type:ticket status:open
+
+    Open tickets
+
+    .EXAMPLE
+    Search-Zendesk -ZendeskConection <Connection> -query type:organization created<2015-05-01
+
+    Organizations created before May 1, 2015
+
+    .EXAMPLE
+    Search-Zendesk -ZendeskConection <Connection> -query status<solved requester:user@domain.com type:ticket
+    
+    Unsolved tickets requested by user@domain.com
+
+    .EXAMPLE
+    Search-Zendesk -ZendeskConection <Connection> -query type:user tags:premium_support
+
+    Users tagged with premium_support
+
+    .EXAMPLE
+    Search-Zendesk -ZendeskConection <Connection> -query created>2012-07-17 type:ticket organization:"MD Photo"
+    
+    Tickets created in the MD Photo org after July 17, 2012
+    
+    .INPUTS
+    Inputs to this cmdlet (if any)
+    
+    .OUTPUTS
+    A ZendeskSearchResult Object    
+    .NOTES
+    This function is not yet complete. 
+    
+    .COMPONENT
+    PS-Zendesk
+#>
+Function Search-Zendesk
+{
+  [CmdletBinding()]
+  [Alias()]
+  [OutputType('ZendeskSearchResultget')]
+  Param
+  (
+    # A ZendeskConnection object
+    [Parameter(Mandatory = $true,
+        ValueFromPipelineByPropertyName = $true,
+    Position = 0)]
+    $ZendeskConnection,
+
+    # Search String
+    [string]$Query
+  )
+  Begin
+  {
+    $Headers = $ZendeskConnection.ZendeskHeaders
+  }
+  process
+  {
+    #GET /api/v2/search.json?query={search_string}
+    $URI = $ZendeskConnection.ZendeskURI + 'search.json?query=' + $Query
+    return Invoke-RestMethod -Uri "$URI" -Method 'GET' -Headers $Headers
+  }
+  end
+  {
+  }
+}
+
+
